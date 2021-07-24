1d338b05ffe4cace40d0c8dd890f9963982ed147
janamiliv <36748903+janamiliv@users.noreply.github.com>
2021-07-24 23:22:19 +0200

    Categories listing and add

--- laravelproject/resources/assets/js/components/Header.js
+++ laravelproject/resources/assets/js/components/Header.js
@@ -1,11 +1,62 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Link, Route } from 'react-router-dom';
+import Home from './Home';
+import About from './About';
+import Category from './category/Index';
 
 export default class Header extends Component {
     render() {
         return (
-            <div>
-                This is header!
-            </div>
+            <Router>
+                <div>
+                    {/* <Link to="/">Home</Link>
+                <Link to="/about">About us</Link> */}
+
+                    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+                        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                            <span className="navbar-toggler-icon"></span>
+                        </button>
+
+                        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                            <ul className="navbar-nav mr-auto">
+                                <li className="nav-item active">
+                                    <Link className="nav-link" to="/">Home </Link>
+                                </li>
+                                <li className="nav-item">
+                                    <Link className="nav-link" to="/about">About us</Link>
+                                </li>
+                                
+                                <li className="nav-item">
+                                    <Link className="nav-link" to="/category">Category</Link>
+                                </li>
+                                {/* <li class="nav-item dropdown">
+                                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                    Dropdown
+                                    </a>
+                                    <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                                    <a class="dropdown-item" href="#">Action</a>
+                                    <a class="dropdown-item" href="#">Another action</a>
+                                    <div class="dropdown-divider"></div>
+                                    <a class="dropdown-item" href="#">Something else here</a>
+                                    </div>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link disabled" href="#">Disabled</a>
+                                </li> */}
+                            </ul>
+                            <form className="form-inline my-2 my-lg-0">
+                                <input className="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search" />
+                                <button className="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+                            </form>
+                        </div>
+                    </nav>
+
+                    {/* exact path znaci da ide na tu putanju ne dodaje samo na postoecu */}
+                    <Route exact path='/' component={Home} />
+                    <Route exact path='/about' component={About} />
+                    <Route exact path='/category' component={Category} />
+                </div>
+            </Router>
         );
     }
 }
--- laravelproject/resources/assets/js/components/Index.js
+++ laravelproject/resources/assets/js/components/Index.js
@@ -9,18 +9,8 @@ export default class Index extends Component {
         return (
             <div className="container">
             <Header/>
-                <div className="row justify-content-center">
-                    <div className="col-md-8">
-                        <div className="card">
-                            <div className="card-header">Landing Component</div>
-
-                            <div className="card-body">
-                                I'm a landing component!
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <Footer/>
+                
+            <Footer/>
             </div>
         );
     }
